ddb4af8566d100a24a2dc64e75646d8d7f224a55
Axel Christ <adracus@gmail.com>
2018-06-13 23:06:02 +0200

    Implement arity checks

--- src/HLisp/Stdlib.hs
+++ src/HLisp/Stdlib.hs
@@ -1,17 +1,50 @@
 module HLisp.Stdlib where
 
+import Text.Printf
 import HLisp.Data
 import qualified HLisp.Eval as Eval
 import qualified Data.Map as Map
 
-def :: Atom
-def = SpecialForm (\b [Ident i, v] -> fmap (\(b, v) -> (Map.insert i v b, v)) $ Eval.eval b v)
+type HFunction0 = Bindings -> Either String Res
+type HFunction1 = Bindings -> Atom -> Either String Res
+type HFunction2 = Bindings -> Atom -> Atom -> Either String Res
+type HFunction3 = Bindings -> Atom -> Atom -> Atom -> Either String Res
 
-eval :: Atom
-eval = SpecialForm (\b [a] -> Eval.eval b a)
+arityN :: Int -> HFunction -> HFunction
+arityN n f = \b args ->
+  let len = length args in
+    if len == n
+    then f b args
+    else Left $ printf "Invalid arity: Expected %d arguments but got %d arguments" n len
 
-ifFn :: HFunction
-ifFn b [cond', then', else'] = do
+arity0 :: HFunction0 -> HFunction
+arity0 f = arityN 0 inner
+  where inner b [] = f b
+        inner b _  = error "Invalid state"
+
+arity1 :: HFunction1 -> HFunction
+arity1 f = arityN 1 inner
+  where inner b [a1] = f b a1
+        inner b _    = error "Invalid state"
+
+arity2 :: HFunction2 -> HFunction
+arity2 f = arityN 2 inner
+  where inner b [a1, a2] = f b a1 a2
+        inner b _        = error "Invalid state"
+
+arity3 :: HFunction3 -> HFunction
+arity3 f = arityN 3 inner
+  where inner b [a1, a2, a3] = f b a1 a2 a3
+        inner b _            = error "Invalid state"
+
+defFn :: HFunction2
+defFn b (Ident i) v = fmap (\(b, v) -> (Map.insert i v b, v)) $ Eval.eval b v
+
+evalFn :: HFunction1
+evalFn b a = Eval.eval b a
+
+ifFn :: HFunction3
+ifFn b cond' then' else' = do
   (b, Boolean cond) <- Eval.eval b cond'
   if cond then Eval.eval b then' else Eval.eval b else'
 
@@ -21,23 +54,29 @@ letFn b [List [Ident i, v], body] = do
   (b, r) <- Eval.eval (Map.insert i v b) body
   return (Map.delete i b, r)
 
-errFn :: HFunction
-errFn b [Str msg] = Left msg
+errFn :: HFunction1
+errFn b (Str msg) = Left msg
 
-plusFn :: HFunction
-plusFn b [Number n1, Number n2] = Right (b, Number $ n1 + n2)
+plusFn :: HFunction2
+plusFn b (Number n1) (Number n2) = Right (b, Number $ n1 + n2)
+
+def :: Atom
+def = SpecialForm $ arity2 defFn
+
+eval :: Atom
+eval = SpecialForm $ arity1 evalFn
 
 if' :: Atom
-if' = SpecialForm ifFn
+if' = SpecialForm $ arity3 ifFn
 
 let' :: Atom
 let' = SpecialForm letFn
 
 err :: Atom
-err = Builtin errFn
+err = Builtin $ arity1 errFn
 
 plus :: Atom
-plus = Builtin plusFn
+plus = Builtin $ arity2 plusFn
 
 true :: Atom
 true = Boolean True
